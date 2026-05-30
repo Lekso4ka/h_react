70a5d85903ecd9a6e31bfd6a0e781513889569c2
surnachevaad <Aleksandra.Surnacheva@lukoil.com>
2026-05-30 20:05:14 +0300

    upd

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const exp = require("express")
 const app = exp()
 app.use(exp.static("./static"))
 
-app.get("*", (q, s) => s.sendFile(path.resolve(process.cwd(), "static", "index.html")))
+app.get("*", (q, s) => s.sendFile(path.join(__dirname, "static", "index.html")))
 
 app.listen(2026)
